458ddcca293a9c3260c2d2f78fac9bf8ce5393c1
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-10-02 20:42:48 -0300

    consulta de Cep Adicionado

--- js/script.js
+++ js/script.js
@@ -24,14 +24,13 @@ joke();
 if(document.documentElement.classList.contains('cripto')) {
   const span = document.querySelector('.content span')
 
-  setInterval(btc(), 20000);
+  setInterval(btc, 20000);
   
   function btc() {
     fetch("https://blockchain.info/ticker")
     .then(response => {
     response.json()
     .then(obj => {
-      console.log(obj);
       span.innerText = obj.BRL.buy + ',00 R$'
       })
     })
@@ -40,6 +39,24 @@ if(document.documentElement.classList.contains('cripto')) {
 }
 
 
-const input = docuem
+if(document.documentElement.classList.contains('cep')){
+  
+  const input = document.querySelector('.content input');
 
+  input.addEventListener('change', muda);
 
+  function muda() {
+    const value = input.value;
+    buscaCep(value);
+  };
+
+  function buscaCep(cep) {
+    const pre = document.querySelector('.content pre'); 
+    const url = fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    url.then(response => response.text()
+    .then(obj => pre.innerText = obj.replace(/"/g, '')
+    .replace(/"/g, '').replace('{', '').replace('}','')
+    .replace(/,/g,'')) 
+    )
+  }
+}
